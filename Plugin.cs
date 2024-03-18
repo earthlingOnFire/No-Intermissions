405eef33e917abeb65a04e10e28f8120aaa23b2a
earthlingOnFire <78956999+earthlingOnFire@users.noreply.github.com>
2024-03-18 23:22:55 +0000

    First commit

---
+++ Plugin.cs
@@ -0,0 +1,34 @@
+﻿using BepInEx;
+using HarmonyLib;
+
+namespace NoIntermissions;
+
+[BepInPlugin(PLUGIN_GUID, PLUGIN_NAME, PLUGIN_VERSION)]
+public class Plugin : BaseUnityPlugin {	
+  public const string PLUGIN_GUID = "com.earthlingOnFire.NoIntermissions";
+  public const string PLUGIN_NAME = "No Intermissions";
+  public const string PLUGIN_VERSION = "1.0.0";
+
+  private void Start() {
+    new Harmony(PLUGIN_GUID).PatchAll();
+  }
+}
+
+[HarmonyPatch]
+public static class Patches {
+  [HarmonyPrefix]
+  [HarmonyPatch(typeof(SceneHelper), nameof(SceneHelper.LoadScene))]
+  private static bool SceneHelper_LoadScene_Prefix(string sceneName) {
+    if (sceneName == "Intermission1") {
+      SceneHelper.LoadScene("Level 4-1");
+      return false;
+    }
+    if (sceneName == "Intermission2") {
+      SceneHelper.LoadScene("Level 7-1");
+      return false;
+    }
+    return true;
+  }
+}
+
+
---
+++ package/README.md
@@ -0,0 +1,7 @@
+WHAT DOES THIS MOD DO?
+- This mod skips the intermission cutscenes that happen at the end of each act.
+- This means that, after 3-2 ends, you will go directly to 4-1, skipping the intermission cutscene and the "Insert Disc 2" part. Likewise, when you finish 6-2, you will go to 7-1 directly.
+
+HOW DO I INSTALL THIS MOD MANUALLY?
+- First, install BepInEx. If you have used Ultrakill mods in the past, you should probably have it installed already.
+- Then, simply drag the "NoIntermissions.dll" file to the BepInEx/plugins folder in your game files.
